b8532b3f88eba79b26398f16a0481fadd898a327
- <5144598+-@users.noreply.github.com>
2021-05-21 00:18:18 +0300

    update

--- tools/markdown.js
+++ tools/markdown.js
@@ -1,33 +1,37 @@
 // I used this to convert puzzle descriptions to markdown
 // the code supposed to be pasted into the browser console
 
-const mdEl = (el, ul) => {
-  switch (el.tagName) {
-    case 'BR':   return '\n'
-    case 'H2':   return `\n\n# ${el.textContent.replace(/-+ | -+/g, '')}\n\n`
-    case 'P':    return `\n\n${mdNodes(el.childNodes, ul)}\n\n`
-    case 'UL':   return mdNodes(el.childNodes, ul + 1)
-    case 'LI':   return `\n${'  '.repeat(ul)}- ${mdNodes(el.childNodes, ul)}`
-    case 'PRE':  return `\n\n\`\`\`\n${el.textContent}\`\`\`\n\n`
-    case 'CODE': return `\`${el.textContent}\``
-    case 'EM':   return `**${mdNodes(el.childNodes, ul)}**`
-    case 'A':    return `[${mdNodes(el.childNodes, ul)}](${el.href})`
-    default:     return mdNodes(el.childNodes, ul)
+const mdUl = (n, ul) => {
+  const md = mdNodes(n.children, ul + 1).trimEnd()
+  return ul ? `\n${md}` : `${md}\n\n`
+}
+
+const mdEl = (n, ul) => {
+  switch (n.tagName) {
+    case 'BR': return '\n'
+    case 'UL': return mdUl(n, ul)
+    case 'LI': return `${'  '.repeat(ul - 1)}- ${mdNodes(n.childNodes, ul)}\n`
+    case 'H2': return `# ${n.textContent.replace(/-+ | -+/g, '')}\n\n`
+    case 'PRE': return `\`\`\`\n${n.textContent}\`\`\`\n\n`
+    case 'CODE': return `\`${n.textContent}\``
+    case 'EM': return `**${mdNodes(n.childNodes, ul)}**`
+    case 'A': return `[${mdNodes(n.childNodes, ul)}](${n.href})`
+    case 'P': return `${mdNodes(n.childNodes, ul)}\n\n`
+    default: return mdNodes(n.childNodes, ul)
   }
 }
 
-const mdNode = (node, ul) => {
-  switch (node.nodeType) {
-    case Node.ELEMENT_NODE: return mdEl(node, ul)
-    case Node.TEXT_NODE:    return node.data.replace(/\n/g, '')
+const mdNode = (n, ul) => {
+  switch (n.nodeType) {
+    case Node.ELEMENT_NODE: return mdEl(n, ul)
+    case Node.TEXT_NODE: return n.data.replace(/\n/g, '')
   }
 }
 
-const mdNodes = (nodes, ul = -1) =>
+const mdNodes = (nodes, ul = 0) =>
   [...nodes].map(n => mdNode(n, ul)).join('')
 
 const md = mdNodes($$('article'))
-  .replace(/\n\s*\n/g, '\n\n')
   .replace(/\*\*`(.+?)`\*\*/g, '`$1`')
   .trim()
 

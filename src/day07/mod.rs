316378d6c92464f0c52a104bdda3f23dd044a4c2
- <5144598+-@users.noreply.github.com>
2021-05-21 10:20:26 +0300

    update

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -2,47 +2,48 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
-type Parsed<'a> = HashMap<&'a str, Vec<(usize, &'a str)>>;
+type Parsed<'a> = HashMap<&'a str, Vec<(&'a str, usize)>>;
 
 fn parse(input: &str) -> Option<Parsed> {
   let mut map = HashMap::new();
   for line in input.lines() {
     let mut contains = Vec::new();
-    let mut bags = line.match_indices(" bag");
-    let nums = line.match_indices(|c| matches!(c, '0'..='9'));
-    let bag = &line[..bags.next()?.0];
-    for (lo, hi) in nums.zip(bags) {
+    let mut hi = line.match_indices(" bag");
+    let lo = line.match_indices(|c| matches!(c, '0'..='9'));
+    let bag = &line[..hi.next()?.0];
+    for (lo, hi) in lo.zip(hi) {
       let (qty, bag) = line[lo.0..hi.0].split_once(' ')?;
-      contains.push((qty.parse().ok()?, bag));
+      contains.push((bag, qty.parse().ok()?));
     }
     map.insert(bag, contains);
   }
   Some(map)
 }
 
-fn part1(parsed: &Parsed, bag: &str) -> usize {
+fn part1(parsed: &mut Parsed, bag: &str) -> usize {
   let mut count = 0;
-  let mut counted = HashMap::new();
   let mut stack = vec![bag];
   while let Some(bag) = stack.pop() {
-    for (&k, v) in parsed {
-      if v.iter().any(|&(_, v)| v == bag) {
-        counted.entry(k).or_insert_with(|| {
+    parsed.retain(|&k, v| {
+      match v.iter().all(|&(b, _)| b != bag) {
+        true => true,
+        false => {
           stack.push(k);
           count += 1;
-        });
+          false
+        }
       }
-    }
+    });
   }
   count
 }
 
 fn part2(parsed: &Parsed, bag: &str) -> usize {
   let mut count = 0;
-  let mut stack = vec![(1, bag)];
-  while let Some((qty1, bag)) = stack.pop() {
-    for &(qty2, bag) in &parsed[bag] {
-      stack.push((qty1 * qty2, bag));
+  let mut stack = vec![(bag, 1)];
+  while let Some((bag, qty1)) = stack.pop() {
+    for &(bag, qty2) in &parsed[bag] {
+      stack.push((bag, qty1 * qty2));
     }
     count += qty1;
   }
@@ -53,8 +54,8 @@ pub fn run(mut file: File) -> (usize, usize) {
   let mut input = String::new();
   file.read_to_string(&mut input).unwrap();
 
-  let parsed = parse(&input).unwrap();
-  let part1 = part1(&parsed, "shiny gold");
+  let mut parsed = parse(&input).unwrap();
+  let part1 = part1(&mut parsed, "shiny gold");
   let part2 = part2(&parsed, "shiny gold");
 
   (part1, part2)

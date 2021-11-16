48bd3db2e7710f2b60d78d3e736fa95a120f15f6
- <5144598+-@users.noreply.github.com>
2021-11-16 12:16:37 +0300

    update

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,53 +1,53 @@
 pub fn run(input: &str) -> (usize, usize) {
   let mut lines = input.lines();
-  let depart_time = lines.next().unwrap().parse::<isize>().unwrap();
+  let depart_time = lines.next().unwrap().parse::<i64>().unwrap();
   let buses = lines.next().unwrap();
 
-  let buses = buses.split(',').enumerate()
-    .filter_map(|(i, id)| Some((i as isize, id.parse().ok()?)))
+  let buses = buses.split(',')
+    .enumerate()
+    .filter_map(|(i, id)| Some((i, id.parse().ok()?)))
+    .map(|(i, id)| (id - i as i64, id))
     .collect::<Vec<_>>();
 
   let part1 = buses.iter()
-    .map(|(_, bus)| (bus, bus - depart_time % bus))
-    .reduce(|a, b| if a.1 < b.1 { a } else { b })
-    .map(|(bus, arrives_in)| bus * arrives_in)
+    .map(|(_, bus)| (bus - depart_time % bus, bus))
+    .reduce(|a, b| if a.0 < b.0 { a } else { b })
+    .map(|(arrives_in, bus)| arrives_in * bus)
     .unwrap() as usize;
 
-  let part2 = {
-    let (rs, ms) = buses.into_iter()
-      .map(|(i, id)| (id - i, id))
-      .unzip::<_, _, Vec<_>, Vec<_>>();
-
-    crt(&rs, &ms).unwrap() as usize
-  };
+  let part2 = crt(&buses).unwrap() as usize;
 
   (part1, part2)
 }
 
-/// greatest common divisor (extended euclidean algorithm)
-fn gcd(a: isize, b: isize) -> (isize, isize, isize) {
-  match a {
-    0 => (b, 0, 1),
-    _ => {
-      let (g, x, y) = gcd(b.rem_euclid(a), a);
-      (g, y - b.div_euclid(a) * x, x)
-    }
-  }
+/// chinese remainder theorem
+fn crt(pairs: &[(i64, i64)]) -> Option<i64> {
+  let product = pairs.iter()
+    .map(|&(_, m)| m)
+    .product::<i64>();
+
+  pairs.iter()
+    .map(|&(r, m)| (r, m, product / m))
+    .map(|(r, m, p)| Some(r * p * inv(p, m)?))
+    .sum::<Option<i64>>()
+    .map(|sum| sum % product)
 }
 
 /// modular multiplicative inverse
-fn mmi(x: isize, n: isize) -> Option<isize> {
+fn inv(x: i64, n: i64) -> Option<i64> {
   match gcd(x, n) {
-    (1, x, _) => Some(x.rem_euclid(n)),
+    (1, x, _) => Some(x % n),
     _ => None
   }
 }
 
-/// chinese remainder theorem
-fn crt(residues: &[isize], modulii: &[isize]) -> Option<isize> {
-  let product = modulii.iter().product::<isize>();
-  residues.iter().zip(modulii)
-    .map(|(&r, &m)| (r, m, product / m))
-    .try_fold(0, |acc, (r, m, p)| Some(acc + r * mmi(p, m)? * p))
-    .map(|sum| sum % product)
+/// greatest common divisor (extended euclidean algorithm)
+fn gcd(a: i64, b: i64) -> (i64, i64, i64) {
+  match a {
+    0 => (b, 0, 1),
+    _ => {
+      let (g, x, y) = gcd(b % a, a);
+      (g, y - b / a * x, x)
+    }
+  }
 }

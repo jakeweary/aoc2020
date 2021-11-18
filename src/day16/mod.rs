894419c58436542eceeee84b46f1dfd7b0f042f0
- <5144598+-@users.noreply.github.com>
2021-11-18 18:38:36 +0300

    update

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -10,7 +10,7 @@ pub fn run(input: &str) -> (usize, usize) {
   let (invalid, valid) = others.into_iter()
     .partition::<Vec<_>, _>(|t| t.is_invalid(&rules));
 
-  let rule_to_field_map = rule_to_field_map(valid, &rules);
+  let rule_to_field_map = rule_to_field_map(&rules, valid);
 
   let part1 = invalid.iter()
     .flat_map(|t| t.invalid_values(&rules))
@@ -18,7 +18,7 @@ pub fn run(input: &str) -> (usize, usize) {
 
   let part2 = rules.into_iter().enumerate()
     .filter(|(_, rule)| rule.name().starts_with("departure"))
-    .map(|(i, _)| mine.field(rule_to_field_map[i]) as usize)
+    .map(|(rule, _)| mine.field(rule_to_field_map[rule]) as usize)
     .product::<usize>();
 
   (part1, part2)
@@ -26,47 +26,36 @@ pub fn run(input: &str) -> (usize, usize) {
 
 fn parse(input: &str) -> Option<(Vec<Rule<'_>>, Ticket, Vec<Ticket>)> {
   let mut blocks = input.split("\n\n").map(str::lines);
-
-  let rules = blocks.next()?
-    .map(Rule::parse)
-    .collect::<Option<_>>()?;
-
-  let mine = blocks.next()?
-    .nth(1)
-    .and_then(Ticket::parse)?;
-
-  let others = blocks.next()?
-    .skip(1)
-    .map(Ticket::parse)
-    .collect::<Option<_>>()?;
-
+  let rules = blocks.next()?.map(Rule::parse).collect::<Option<_>>()?;
+  let mine = blocks.next()?.nth(1).and_then(Ticket::parse)?;
+  let others = blocks.next()?.skip(1).map(Ticket::parse).collect::<Option<_>>()?;
   Some((rules, mine, others))
 }
 
-fn rule_to_field_map(tickets: Vec<Ticket>, rules: &[Rule<'_>]) -> [usize; FIELDS] {
+fn rule_to_field_map(rules: &[Rule<'_>], tickets: Vec<Ticket>) -> [usize; FIELDS] {
   let fields = transpose(tickets).unwrap();
 
   let mut nth_field_matches_n_rules = [0; FIELDS];
   let mut nth_rule_matches_n_fields = [0; FIELDS];
-  let mut rule_to_field_map = [0; FIELDS];
+  let mut map = [0; FIELDS];
 
-  for (r, rule) in rules.iter().enumerate() {
-    for (f, field) in fields.iter().enumerate() {
+  for (n_rules, field) in nth_field_matches_n_rules.iter_mut().zip(fields) {
+    for (n_fields, rule) in nth_rule_matches_n_fields.iter_mut().zip(rules) {
       if field.iter().all(|&v| rule.matches(v)) {
-        nth_field_matches_n_rules[f] += 1;
-        nth_rule_matches_n_fields[r] += 1;
+        *n_rules += 1;
+        *n_fields += 1;
       }
     }
   }
 
-  for (map, &n) in rule_to_field_map.iter_mut().zip(&nth_rule_matches_n_fields) {
+  for (map, &n) in map.iter_mut().zip(&nth_rule_matches_n_fields) {
     let correlated = 1 + FIELDS - n;
     *map = nth_field_matches_n_rules.iter()
       .position(|&n| n == correlated)
       .unwrap();
   }
 
-  rule_to_field_map
+  map
 }
 
 fn transpose<T, M, V>(matrix: M) -> Option<Vec<Vec<T>>>
